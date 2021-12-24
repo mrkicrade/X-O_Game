3c3d4a6c2d9540b5161b7378c760fa53cbbce58b
Rade Mrkic <ramrkic@pio.srb>
2021-12-24 08:50:44 +0100

    finnally commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let container = document.querySelector('.container');
 let result = document.querySelector('.result');
+let winner = 0;
 
 createGrid();
 
@@ -20,36 +21,40 @@ let xo = "O";
 
 for (let i = 0; i < box.length; i++) {
     const div = box[i];
-    div.addEventListener('click', dugme)
+    div.addEventListener('click', play)
 }
 
-function dugme() {
+function play() {
+    winner ++;
+    console.log(winner);
     (xo == "O") ? xo = "X" : xo = "O";
     this.innerHTML = xo;
-    this.removeEventListener('click', dugme);
+    this.removeEventListener('click', play);
     
     checkLines();
+    draw();
 }
 function checkLines() {
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
-        if (line[0].innerHTML == line[1].innerHTML && line[1].innerHTML == line[2].innerHTML && line[0].innerHTML !="" ) {
-            // console.log('1');
+        if (line[0].innerHTML == line[1].innerHTML && line[1].innerHTML == line[2].innerHTML && line[0].innerHTML !="") {
             for (let i = 0; i < line.length; i++) {
                 line[i].style.background = 'red';
                 if (xo == 'X') {
                     result.innerHTML = '<h1>Pobednik je igrac X</h1>';
+                    winner ++;
+                    console.log(winner);
                 } else {
                     result.innerHTML = '<h1>Pobednik je igrac O</h1>';
+                    winner ++;
+                    console.log(winner);
                 }
             }
             for (let k = 0; k < box.length; k++) {
-                box[k].removeEventListener('click', dugme);
+                box[k].removeEventListener('click', play);
             }   
             break;
-        } else {
-            result.innerHTML = '<h1>Nema pobednika</h1>';
-        }
+        } 
     }
 }
 
@@ -57,9 +62,13 @@ function createGrid(){
     let text = "";
     for (let i = 0; i < 9; i++) {
         text +='<div class="box"></div>';
-        // text +='<div class="box"><i class="fab fa-adn"></i></div>';
     }
-    // console.log(text);
     container.innerHTML = text;
+}
+
+function draw () {
+    if (winner == 9) {
+        result.innerHTML = '<h1>Nema pobednika</h1>'
+    }
 }
 
